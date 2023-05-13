9eb10ed3c638ab2699b4a9845731f7699d5eceb3
beproactivegr <social@beproactive.gr>
2023-05-14 01:50:13 +0300

    tooltips added in dashboard

--- it-polygraph/frontend/src/menu.js
+++ it-polygraph/frontend/src/menu.js
@@ -25,7 +25,7 @@ function loadDashboard() {
                         <div class="row">
                             <div class="col-5">
                               <b>Computer Hostname</b>&nbsp;
-                              <i class="fa-regular fa-circle-question" data-toggle="tooltip" data-placement="right" title="The name of your computer."></i><br>
+                              <i class="fa-regular fa-circle-question mytooltip" data-toggle="tooltip" data-placement="right" title="The name of your computer."></i><br>
                               <i class="fas fa-computer"></i><span class="data-result" id="hostname-result"></span>
                             </div>
                             <div class="col-5">
@@ -39,7 +39,7 @@ function loadDashboard() {
                     <div class="col-6 border border-dark infobox">
                         <div class="row">
                             <div class="col-5">
-                              <b>Local IP Address</b>&nbsp;
+                              <b>Local IP Address</b>&nbsp
                               <i class="fa-regular fa-circle-question mytooltip" data-toggle="tooltip" data-html="true" data-placement="right" title="A Local IP address is the unique identifier that is assigned to &#013;your device within your local network. It is not visible to the outer world, &#013;but it is required in order to use the network."></i><br>
                               <i class="fas fa-network-wired"></i><span class="data-result" id="ipaddress-result"></span>
                             </div>
@@ -56,7 +56,8 @@ function loadDashboard() {
                     <div class="col-6 border border-dark infobox">
                         <div class="row">
                             <div class="col-5">
-                              <b>Internet IP Address</b>&nbsp;<i class="fa-regular fa-circle-question"></i><br>
+                              <b>Internet IP Address</b>&nbsp
+                              <i class="fa-regular fa-circle-question mytooltip" data-toggle="tooltip" data-html="true" data-placement="right" title="An Internet (IP) address is a unique numerical identifier for &#013;every device or network that connects to the internet."></i><br>
                               <i class="fas fa-globe"></i><span class="data-result" id="publicaddress-result"></span>
                             </div>
                             <div class="col-5">
@@ -70,7 +71,8 @@ function loadDashboard() {
                     <div class="col-6 border border-dark infobox">
                         <div class="row">
                             <div class="col-8">
-                              <b>NMAP Status</b>&nbsp;<i class="fa-regular fa-circle-question"></i><br>
+                              <b>NMAP Status</b>&nbsp
+                              <i class="fa-regular fa-circle-question mytooltip" data-toggle="tooltip" data-html="true" data-placement="right" title="Nmap ('Network Mapper') is a free and open source utility for network discovery and security auditing. &#013;Many systems and network administrators also find it useful for tasks such as network inventory, &#013;managing service upgrade schedules, and monitoring host or service uptime."></i><br>
                               <i class="fas fa-cube"></i>
                                 <span class="data-result" id="nmap-result"></span>
 <!--                                <span id="nmap-download-result"></span>-->
